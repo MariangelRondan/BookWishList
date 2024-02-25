9f3475d82f202702c14ed8e93571db1afd203c16
MariangelRondan <rondanmariangel@gmail.com>
2024-02-25 18:23:45 -0300

    primer commit

--- BookWishList/Components/Models/Book.cs
+++ BookWishList/Components/Models/Book.cs
@@ -2,5 +2,12 @@
 {
     public class Book
     {
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author{ get; set; }
+
+        public bool Read { get; set; }
     }
 }
